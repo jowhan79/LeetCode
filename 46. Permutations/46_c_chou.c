83f1baf82a9a390596f36d469744b9b9c0dee075
LittleAnimal <jennifera06@gmail.com>
2017-11-28 16:31:29 +0800

    Update 46_c_chou.c

--- 46. Permutations/46_c_chou.c
+++ 46. Permutations/46_c_chou.c
@@ -1,5 +1,10 @@
 //2017/11/28 Peony
 
+/**
+ * Return an array of arrays of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
 int count, *visited, *tmpList;
 
 inline int nfactorial(int n){
@@ -10,7 +15,7 @@ inline int nfactorial(int n){
 }
 
 void dfs(int **ans, int *nums, int numsSize, int visitID, int visitN){
-    
+    //printf("(nums[%d], %d)->N=%d, C=%d\n", visitID, nums[visitID], visitN, count);
     int i;
     if(numsSize==visitN){
         for(i=0; i<numsSize; i++){
@@ -20,6 +25,7 @@ void dfs(int **ans, int *nums, int numsSize, int visitID, int visitN){
         return;
     }
     for(i=0; i<numsSize; i++){
+        //printf("check %d, %d\n", nums[i], visited[i]);
         if(visited[i]==0){
             tmpList[visitN] = nums[i];
             visited[i]=1;
@@ -27,12 +33,13 @@ void dfs(int **ans, int *nums, int numsSize, int visitID, int visitN){
             visited[i]=0;
         }
     }
+    
 }
 
 int** permute(int* nums, int numsSize, int* returnSize) {
 
     int **ans, pairSize, i, j;
-    int *visited, *tmpList;
+    //int *visited, *tmpList;
     // Initialization
     count = 0;
     pairSize = nfactorial(numsSize);
@@ -40,11 +47,16 @@ int** permute(int* nums, int numsSize, int* returnSize) {
     for(i=0; i<pairSize; i++){
         ans[i] = (int*)malloc(numsSize*sizeof(int));
     }
-    visited = (int*)malloc(numsSize*sizeof(int*));
-    memset(visited, 0, sizeof(visited));
-    tmpList = (int*)malloc(numsSize*sizeof(int*));
+    visited = (int*)malloc(numsSize*sizeof(int));
+    memset(visited, 0, numsSize*sizeof(int));
+    tmpList = (int*)malloc(numsSize*sizeof(int));
+    memset(tmpList, 0, numsSize*sizeof(int));
     // Set value
     dfs(ans, nums, numsSize, 0, 0);
+    free(visited);
+    free(tmpList);
+    visited = NULL;
+    tmpList = NULL;
     *returnSize = pairSize;
     return ans;
 }
